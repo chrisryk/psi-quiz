2cec46fced2b786dac3ae7e309eb53ca4f88a11e
Krzysztof Rykowski <rykowski.krzysztof@gmail.com>
2022-04-07 15:13:20 +0200

    fix scoreboard color

--- Forms/ScoreboardWindow.Designer.cs
+++ Forms/ScoreboardWindow.Designer.cs
@@ -66,6 +66,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
             this.Controls.Add(this.usersListTable);
